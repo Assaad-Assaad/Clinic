4b082207ef953dd00bfb9cfeed934ac034465b49
Assaad <assaad.assaad@student.ehb.be>
2024-12-27 12:42:59 +0100

    Delete the workflow

--- src/test/java/be/ehb/clinic/ClinicApplicationTests.java
+++ src/test/java/be/ehb/clinic/ClinicApplicationTests.java
@@ -3,10 +3,10 @@ package be.ehb.clinic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class ClinicApplicationTests {
 
-   // @Test
+    @Test
     void contextLoads() {
     }
 
