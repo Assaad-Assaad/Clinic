f17269b894c615bb021004a05d2d84380d7d7050
Assaad <assaad.assaad@student.ehb.be>
2024-12-27 01:04:29 +0100

    Testing CI attempt 3

--- src/test/java/be/ehb/clinic/model/NationalRegisterNumberValidatorTest.java
+++ src/test/java/be/ehb/clinic/model/NationalRegisterNumberValidatorTest.java
@@ -1,39 +1,39 @@
-//package be.ehb.clinic.model;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//
-//class NationalRegisterNumberValidatorTest {
-//
-//    static NationalRegisterNumberValidator validator;
-//
-//    @BeforeEach
-//    void setUp() {
-//        validator = new NationalRegisterNumberValidator();
-//    }
-//
-//    @Test
-//    void givenValidNationalRegisterNumber() {
-//        String s = "90031139126";
-//        boolean valid = validator.isValid(s, null);
-//        assertTrue(valid);
-//    }
-//
-//    @Test
-//    void givenInvalidNationalRegisterNumber() {
-//        String s = "90031139122";
-//        boolean valid = validator.isValid(s, null);
-//        assertFalse(valid);
-//    }
-//
-//    @Test
-//    void givenNullNationalRegisterNumber() {
-//        String s = "";
-//        boolean valid = validator.isValid(s, null);
-//        assertFalse(valid);
-//    }
-//}
+package be.ehb.clinic.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class NationalRegisterNumberValidatorTest {
+
+    static NationalRegisterNumberValidator validator;
+
+    @BeforeEach
+    void setUp() {
+        validator = new NationalRegisterNumberValidator();
+    }
+
+    @Test
+    void givenValidNationalRegisterNumber() {
+        String s = "90031139126";
+        boolean valid = validator.isValid(s, null);
+        assertTrue(valid);
+    }
+
+    @Test
+    void givenInvalidNationalRegisterNumber() {
+        String s = "90031139122";
+        boolean valid = validator.isValid(s, null);
+        assertFalse(valid);
+    }
+
+    @Test
+    void givenNullNationalRegisterNumber() {
+        String s = "";
+        boolean valid = validator.isValid(s, null);
+        assertFalse(valid);
+    }
+}
